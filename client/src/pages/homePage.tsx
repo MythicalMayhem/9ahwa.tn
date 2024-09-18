55f5742363ca549204d1e03636a313b3dfbee31d
MythicalMayhem <ameuroussema@gmail.com>
2024-09-18 09:50:57 +0100

    homepage css

--- client/src/pages/homePage.tsx
+++ client/src/pages/homePage.tsx
@@ -1,24 +1,23 @@
 import { useNavigate } from "react-router-dom";
 import "../styles/homePage.css"
-import { Button, Card } from "react-bootstrap";
 function HomePage() {
   const navigate = useNavigate()
   return <div className="home-page">
-    <header>Home Page</header>
+    <div className="header">Home Page</div>
     <section>
 
-      <Card className="card"  >
-        <Card.Img variant="top" src="imgs/games/chkobba.jpg" height={"150px"} />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
+      <div className="card"  >
+
+        <div className="text">
+          <h1>Card Title</h1>
+          <p>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit.
             sit aut soluta eveniet quis aspernatur perspiciatis blanditiis.
             Obcaecati ab nemo impedit.
-          </Card.Text>
-          <Button variant="primary" onClick={() => navigate("/queue")}> chkoba </Button>
-        </Card.Body>
-      </Card>
+          </p>
+          <button onClick={() => navigate("/queue")}> Queue </button>
+        </div>
+      </div>
 
     </section>
     <footer>Made By Mayhem</footer>
