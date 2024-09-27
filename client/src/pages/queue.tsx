afb339067ea538272cbf593071d8bab9b21c4bf3
MythicalMayhem <ameuroussema@gmail.com>
2024-09-27 15:38:11 +0100

    queue css

--- client/src/pages/queue.tsx
+++ client/src/pages/queue.tsx
@@ -1,35 +1,31 @@
+import '../styles/queue.css'
 import { userStore } from "../stores/user";
 
 function QueuePage() {
   const user = userStore();
 
   const handleQueue = () => {
-    user.queue("nameeee");
-    const q = 10;
-    console.log(q);
-
+    user.queue(String(~~(Math.random() * 55555)));
     if (!user.nickname) return;
   };
-
 
   const handleUnqueue = () => {
     user.unqueue();
   };
 
   return (
-    <div>
-      <strong>username :</strong>
-      {user.nickname || <b style={{ color: "red" }}>Choose nickname</b>}
-      <br />
+    <div className='queue-wrapper'>
+      <strong className='nickname-label'>nickname :</strong>
+      <h1 className='user-name'> {user.nickname || <b className='no-user-name'>Choose nickname</b>}</h1>
+<br />
       <input
         type="text"
-        placeholder="eEEEeeeee"
+        placeholder="mounir"
+        className='user-name-input'
         value={user.nickname || ""}
         onChange={(e) => user.setNickName(e.target.value)}
       />
-      <br />
-      <br />
-      <button onClick={() => (user.queueing ? handleUnqueue() : handleQueue())}>
+      <button onClick={() => (user.queueing ? handleUnqueue() : handleQueue())} className='queued'>
         {user.queueing ? "Leave Queue" : "Queue"}
       </button>
     </div>
